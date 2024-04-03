309e5b8187e0758ae1957f87ef76a91c64a4a98b
Joonas Kesäniemi <joonas.kesaniemi@csc.fi>
2024-04-03 16:24:24 +0300

    Revert user update in startup

--- src/main/java/fi/vm/yti/datamodel/api/StartUpListener.java
+++ src/main/java/fi/vm/yti/datamodel/api/StartUpListener.java
@@ -50,7 +50,7 @@ public class StartUpListener {
         initServiceCategories();
         initOpenSearchIndices();
         initFunctions();
-        groupManagementService.initUsers();
+        groupManagementService.updateUsers();
     }
 
     @Scheduled(cron = "0 */5 * * * *")
